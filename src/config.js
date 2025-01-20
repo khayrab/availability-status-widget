e9bf5895ac56575db242f17451b361c758054368
khayrab <69257859+khayrab@users.noreply.github.com>
2025-01-20 18:21:28 +0300

    Update config.js
    So it works with github secrets

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const GOOGLE_CONFIG = {
-  API_KEY: 'YOUR-NEW-API-KEY', //from google cloud
-  CLIENT_ID: 'YOUR-CLIENT-ID', //from google cloud
-  CALENDAR_ID: 'YOUR-CALENDAR-ID' //email address
+  API_KEY: process.env.REACT_APP_GOOGLE_API_KEY,
+  CLIENT_ID: process.env.REACT_APP_GOOGLE_CLIENT_ID,
+  CALENDAR_ID: process.env.REACT_APP_CALENDAR_ID
 };
