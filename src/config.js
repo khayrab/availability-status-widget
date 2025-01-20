4717a1cc9717fbeec7e9e25f1183b513be0adde1
khayrab <69257859+khayrab@users.noreply.github.com>
2025-01-20 17:29:43 +0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const GOOGLE_CONFIG = {
-  API_KEY: 'YOUR-NEW-API-KEY',
-  CLIENT_ID: 'YOUR-CLIENT-ID',
-  CALENDAR_ID: 'kk@khayrab.com'
+  API_KEY: 'YOUR-NEW-API-KEY', //from google cloud
+  CLIENT_ID: 'YOUR-CLIENT-ID', //from google cloud
+  CALENDAR_ID: 'YOUR-CALENDAR-ID' //email address
 };
