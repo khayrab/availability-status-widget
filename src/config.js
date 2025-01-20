43ca274eada4fc7717bc407289bd0a349f723c02
khayrab <69257859+khayrab@users.noreply.github.com>
2025-01-20 16:54:14 +0300

    Create config.js

---
+++ src/config.js
@@ -0,0 +1,5 @@
+export const GOOGLE_CONFIG = {
+  API_KEY: 'AIzaSyBKDSb_oJG8QyZp8egTDJ2Y7u8xDeqLnC0',
+  CLIENT_ID: '489210230303-sl2hpqla0dum70mnfe9bh4m6piqeb5ms.apps.googleusercontent.com',
+  CALENDAR_ID: 'kk@khayrab.com'
+};
